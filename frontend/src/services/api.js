f4a92e8aa776b582ef49b365b6089c7eb0869085
Hugo Melo <hugomelo742@gmail.com>
2026-03-27 13:04:42 -0300

    ajustando url do backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://sistema-de-agendamento-barbearia-1-oako.onrender.com";
 
 function getToken() {
   return localStorage.getItem("token");
